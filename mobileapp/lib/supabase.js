82d495998f03eb52bcf0cf3f04c6941f38703dbe
MSH <sionghwee101@gmail.com>
2023-05-25 01:14:24 +0800

    s

--- mobileapp/lib/supabase.js
+++ mobileapp/lib/supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const projectUrl = process.env.SUPABASE_PROJECT_URL
-const projectKey = process.env.SUPABASE_PROJECT_KEY
+const projectUrl = "https://nzuhiipfehwxwmcgsgah.supabase.co"
+const projectKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56dWhpaXBmZWh3eHdtY2dzZ2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MDc5ODAsImV4cCI6MjAwMDM4Mzk4MH0.w-yxLViUAATJ-DXH1I_PFZQ1fytbZqv6tO3kDF65Q1I"
 
 export const supabase = createClient(projectUrl, projectKey, {
     auth: {
