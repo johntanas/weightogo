4b451fedb08076a7122817eb74d2eeb991313922
Dominic Cheong <dominiccheong123@gmail.com>
2023-05-28 16:51:42 +0800

    adjusted the layout for the app.

--- mobileapp/app/(home)/_layout.jsx
+++ mobileapp/app/(home)/_layout.jsx
@@ -4,8 +4,10 @@ export default function HomeScreen() {
     
     return (
         <Tabs>
-            <Tabs.Screen name="index" options={{ title: "profile" }} />
+            <Tabs.Screen name="index" options={{ title: "Home" }} />
+            <Tabs.Screen name = "calculator" options = {{title : "Calorie Calculator"}} />
             <Tabs.Screen name="dataset" />
+            <Tabs.Screen name = "settings" />
         </Tabs>
     );
 }
---
+++ mobileapp/app/(home)/calculator.jsx
@@ -0,0 +1,10 @@
+import CalcDisplay from "../../components/CalorieCalculator";
+import {View} from "react-native";
+
+export default function CalcPage(){
+    return(
+        <View>
+            <CalcDisplay />
+        </View>
+    )
+}
