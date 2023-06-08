7d294d78aba88c252a45722d8375457581e5bf47
MSH <sionghwee101@gmail.com>
2023-06-08 18:49:47 +0800

    Incomplete weightlog

--- mobileapp/components/WeightLogger.jsx
+++ mobileapp/components/WeightLogger.jsx
@@ -1,12 +1,13 @@
 import { supabase } from '../lib/supabase';
 import { Text, TextInput, Button, ActivityIndicator } from "react-native-paper";
-import {  FlatList, View } from 'react-native';
+import {DatePickerIOS,FlatList, View } from 'react-native';
 import { useEffect, useState } from 'react';
+import { useAuth } from "../contexts/auth";
 
 export default  function WeightLog() {
   const [weights, setWeight] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
-
+  const { user } = useAuth();
   async function fetchWeight() {
       setRefreshing(true);
       let { data } = await supabase.from('weightData').select('*');
@@ -27,28 +28,39 @@ export default  function WeightLog() {
 
   return (
       <View>
+          <FlatList
+            data={weights}
+            renderItem={({ item }) => <WeightItem weight={item} />}
+            onRefresh={() => setRefreshing(true)}
+            refreshing={refreshing}
+          />
         <AddWeight/>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <FlatList
-              data={weights}
-              renderItem={({ item }) => <WeightItem weight={item} />}
-              onRefresh={() => setRefreshing(true)}
-              refreshing={refreshing}
-            />
-        </View>
       </View>
   );
 }
 function AddWeight(){
-    return <View style={{justifyContent: 'center' }}>
-      <Text>Date: </Text>
-      <TextInput />
-    </View>
+  const { user } = useAuth();
+  const [chosenDate, setChosenDate] = useState(new Date());
+  const [weight, setWeight ] = useState(0);
+  const onSubmit = async () => {
+    console.log(user.id);
+    const { error } = await supabase.from('weightData').upsert({ user_id: user.id,date:chosenDate,weight:weight});
+    console.log(error)
+  }
+    return (
+      <View>
+        <DatePickerIOS date={chosenDate} onDateChange={setChosenDate} mode='date'/>
+        <Text> Weight</Text>
+        <TextInput onChangeText={(value) =>{setWeight(value)}}></TextInput>
+        <Button onPress = {onSubmit}>Submit</Button>
+      </View>
+      )
 }
 function WeightItem({weight}) {
   return (
       <View>
-
+        <Text> {weight.date}</Text>
+        <Text> {weight.weight}</Text>
       </View>
   )
 }
