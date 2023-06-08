b3a96cb7197541a02961062821065ede55918e53
MSH <sionghwee101@gmail.com>
2023-06-08 22:29:06 +0800

    updating the screen + dup entries

--- mobileapp/components/WeightLogger.jsx
+++ mobileapp/components/WeightLogger.jsx
@@ -1,18 +1,20 @@
 import { supabase } from '../lib/supabase';
 import { Text, TextInput, Button, ActivityIndicator } from "react-native-paper";
-import {DatePickerIOS,FlatList, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import {StyleSheet,FlatList, View} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { memo,useEffect, useState } from 'react';
 import { useAuth } from "../contexts/auth";
+import { useRouter } from 'expo-router';
 
 export default  function WeightLog() {
-  const [weights, setWeight] = useState([]);
+  const [weights, setWeights] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
-  const { user } = useAuth();
   async function fetchWeight() {
       setRefreshing(true);
       let { data } = await supabase.from('weightData').select('*');
       setRefreshing(false);
-      setWeight(data);
+      data.sort((a,b)=> a.date>b.date ? 1 : a.date==b.date ?0 :-1);
+      setWeights(data);
   }
 
   useEffect(() => {
@@ -25,15 +27,15 @@ export default  function WeightLog() {
         setRefreshing(false);
       }
   }, [refreshing]);
-
   return (
       <View>
-          <FlatList
-            data={weights}
-            renderItem={({ item }) => <WeightItem weight={item} />}
-            onRefresh={() => setRefreshing(true)}
-            refreshing={refreshing}
-          />
+        <FlatList
+          data={weights}
+          initialNumToRender={10}
+          renderItem={({ item }) => <WeightItem weight={item} />}
+          onRefresh={() => setRefreshing(true)}
+          refreshing={refreshing}
+        />
         <AddWeight/>
       </View>
   );
@@ -41,26 +43,44 @@ export default  function WeightLog() {
 function AddWeight(){
   const { user } = useAuth();
   const [chosenDate, setChosenDate] = useState(new Date());
-  const [weight, setWeight ] = useState(0);
+  const router = useRouter();
+  const [weight, setWeight ] = useState('99');
   const onSubmit = async () => {
-    console.log(user.id);
-    const { error } = await supabase.from('weightData').upsert({ user_id: user.id,date:chosenDate,weight:weight});
-    console.log(error)
+    const floatWeight=parseFloat(weight)
+    const { error } = await supabase.from('weightData').upsert({ user_id: user.id,date:chosenDate,weight:floatWeight});
   }
     return (
       <View>
-        <DatePickerIOS date={chosenDate} onDateChange={setChosenDate} mode='date'/>
-        <Text> Weight</Text>
-        <TextInput onChangeText={(value) =>{setWeight(value)}}></TextInput>
+        <View style={styles.dateContainer}>
+          <Text> Date:</Text>
+          <DateTimePicker value={chosenDate} onChange={(event, date) =>setChosenDate(date)} mode='date'/>
+          <Text> Weight:</Text>
+          <TextInput keyboardType ='numbers-and-punctuation'  onChangeText={(value) =>{setWeight(value)}}></TextInput>
+        </View>
         <Button onPress = {onSubmit}>Submit</Button>
       </View>
       )
 }
 function WeightItem({weight}) {
+  const onSubmit = async () => {
+    const { error } = await supabase
+    .from('weightData')
+    .delete()
+    .eq('id', weight.id)
+    console.log(error)
+  }
   return (
-      <View>
-        <Text> {weight.date}</Text>
-        <Text> {weight.weight}</Text>
+      <View style={styles.dateContainer}>
+        <Text>{weight.date}</Text>
+        <Text>{weight.weight}</Text>
+        <Button onPress = {onSubmit}>Delete</Button>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+  dateContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between'
+  }
+});
